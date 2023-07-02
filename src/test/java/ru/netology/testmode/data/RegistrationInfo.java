acb57fa959e177c5ce070d207ce9354ef6c82318
DAP2106 <pachkovskiid82@gmail.com>
2023-07-02 10:47:23 +0300

    add test

---
+++ src/test/java/ru/netology/testmode/data/RegistrationInfo.java
@@ -0,0 +1,13 @@
+
+package ru.netology.testmode.data;
+
+import lombok.*;
+    @Value
+    public class RegistrationInfo {
+        private String login;
+        private String password;
+        private String status;
+    }
+
+
+}
